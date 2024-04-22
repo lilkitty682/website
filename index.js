6b70b22008956d2e4a60a8405618fe2542b92579
lilkitty682 <74938062+lilkitty682@users.noreply.github.com>
2024-04-22 16:30:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 function coolfunctionyes() {
-  alert("see i told you the button does something completely useless");
+  alert("see i told you the button does something completely useless i didnt but like whatever u shouldve known tsk tsk tsk");
 }
